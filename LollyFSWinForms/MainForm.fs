bf7d8adf419e37a1829de7cc774be0f682c997f7
zwvista <Skyline0515>
2015-06-23 11:14:24 +0900

    Removed TableLayoutPanel

--- LollyFSWinForms/MainForm.fs
+++ LollyFSWinForms/MainForm.fs
@@ -10,109 +10,39 @@ type MainForm() as this =
     inherit Form()
     do this.InitializeComponent()
 
-    [<DefaultValue>]val mutable components: System.ComponentModel.Container;
+    [<DefaultValue>]val mutable private components: System.ComponentModel.Container;
     override this.Dispose(disposing) =
         if (disposing && (match this.components with null -> false | _ -> true)) then
             this.components.Dispose();
         base.Dispose(disposing)
 
-    member this.InitializeComponent() =
+    member private this.InitializeComponent() =
         this.components <- new System.ComponentModel.Container();
-        this.tableLayoutPanel1 <- new System.Windows.Forms.TableLayoutPanel();
-        this.langComboBox <- new System.Windows.Forms.ComboBox();
         this.mLANGUAGEBindingSource <- new System.Windows.Forms.BindingSource(this.components);
-        this.dictComboBox <- new System.Windows.Forms.ComboBox();
         this.mDICTALLBindingSource <- new System.Windows.Forms.BindingSource(this.components);
-        this.label2 <- new System.Windows.Forms.Label();
-        this.label3 <- new System.Windows.Forms.Label();
         this.panel1 <- new System.Windows.Forms.Panel();
         this.searchButton <- new System.Windows.Forms.Button();
         this.wordTextBox <- new System.Windows.Forms.TextBox();
         this.label1 <- new System.Windows.Forms.Label();
         this.dictWebBrowser <- new System.Windows.Forms.WebBrowser();
-        this.tableLayoutPanel1.SuspendLayout();
+        this.panel2 <- new System.Windows.Forms.Panel();
+        this.langComboBox <- new System.Windows.Forms.ComboBox();
+        this.dictComboBox <- new System.Windows.Forms.ComboBox();
+        this.label2 <- new System.Windows.Forms.Label();
+        this.label3 <- new System.Windows.Forms.Label();
         ((this.mLANGUAGEBindingSource) :> (System.ComponentModel.ISupportInitialize)).BeginInit();
         ((this.mDICTALLBindingSource) :> (System.ComponentModel.ISupportInitialize)).BeginInit();
         this.panel1.SuspendLayout();
+        this.panel2.SuspendLayout();
         this.SuspendLayout();
-        // 
-        // tableLayoutPanel1
-        // 
-        this.tableLayoutPanel1.ColumnCount <- 4;
-        ignore(this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle()));
-        ignore(this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0f)));
-        ignore(this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle()));
-        ignore(this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0f)));
-        this.tableLayoutPanel1.Controls.Add(this.langComboBox, 1, 0);
-        this.tableLayoutPanel1.Controls.Add(this.dictComboBox, 3, 0);
-        this.tableLayoutPanel1.Controls.Add(this.label2, 0, 0);
-        this.tableLayoutPanel1.Controls.Add(this.label3, 2, 0);
-        this.tableLayoutPanel1.Dock <- System.Windows.Forms.DockStyle.Bottom;
-        this.tableLayoutPanel1.Location <- new System.Drawing.Point(0, 366);
-        this.tableLayoutPanel1.Margin <- new System.Windows.Forms.Padding(4);
-        this.tableLayoutPanel1.Name <- "tableLayoutPanel1";
-        this.tableLayoutPanel1.RowCount <- 1;
-        ignore(this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0f)));
-        this.tableLayoutPanel1.Size <- new System.Drawing.Size(657, 31);
-        this.tableLayoutPanel1.TabIndex <- 0;
-        // 
-        // langComboBox
-        // 
-        this.langComboBox.DataSource <- this.mLANGUAGEBindingSource;
-        this.langComboBox.DisplayMember <- "LANGNAME";
-        this.langComboBox.DropDownStyle <- System.Windows.Forms.ComboBoxStyle.DropDownList;
-        this.langComboBox.FormattingEnabled <- true;
-        this.langComboBox.Location <- new System.Drawing.Point(86, 4);
-        this.langComboBox.Margin <- new System.Windows.Forms.Padding(4);
-        this.langComboBox.Name <- "langComboBox";
-        this.langComboBox.Size <- new System.Drawing.Size(180, 24);
-        this.langComboBox.TabIndex <- 2;
-        this.langComboBox.ValueMember <- "LANGID";
-        let langComboBox_SelectionChangeCommitted = (fun e ->
-            this.mDICTALLBindingSource.DataSource <- LollyDB.DictAll_GetDataByLang(this.langComboBox.SelectedValue :?> int64);
-        )
-        this.langComboBox.SelectionChangeCommitted.Add(langComboBox_SelectionChangeCommitted);
         // 
         // mLANGUAGEBindingSource
         // 
         this.mLANGUAGEBindingSource.DataSource <- typedefof<LollyShared.MLANGUAGE>;
         // 
-        // dictComboBox
-        // 
-        this.dictComboBox.DataSource <- this.mDICTALLBindingSource;
-        this.dictComboBox.DisplayMember <- "DICTNAME";
-        this.dictComboBox.DropDownStyle <- System.Windows.Forms.ComboBoxStyle.DropDownList;
-        this.dictComboBox.FormattingEnabled <- true;
-        this.dictComboBox.Location <- new System.Drawing.Point(417, 4);
-        this.dictComboBox.Margin <- new System.Windows.Forms.Padding(4);
-        this.dictComboBox.Name <- "dictComboBox";
-        this.dictComboBox.Size <- new System.Drawing.Size(180, 24);
-        this.dictComboBox.TabIndex <- 3;
-        this.dictComboBox.ValueMember <- "URL";
-        // 
         // mDICTALLBindingSource
         // 
         this.mDICTALLBindingSource.DataSource <- typedefof<LollyShared.MDICTALL>;
-        // 
-        // label2
-        // 
-        this.label2.AutoSize <- true;
-        this.label2.Location <- new System.Drawing.Point(4, 8);
-        this.label2.Margin <- new System.Windows.Forms.Padding(4, 8, 4, 4);
-        this.label2.Name <- "label2";
-        this.label2.Size <- new System.Drawing.Size(74, 16);
-        this.label2.TabIndex <- 0;
-        this.label2.Text <- "Language:";
-        // 
-        // label3
-        // 
-        this.label3.AutoSize <- true;
-        this.label3.Location <- new System.Drawing.Point(330, 8);
-        this.label3.Margin <- new System.Windows.Forms.Padding(4, 8, 4, 4);
-        this.label3.Name <- "label3";
-        this.label3.Size <- new System.Drawing.Size(79, 16);
-        this.label3.TabIndex <- 1;
-        this.label3.Text <- "Dictionary:";
         // 
         // panel1
         // 
@@ -134,11 +64,7 @@ type MainForm() as this =
         this.searchButton.TabIndex <- 2;
         this.searchButton.Text <- "Search";
         this.searchButton.UseVisualStyleBackColor <- true;
-        this.searchButton.Click.Add(fun e ->
-            let row = this.mDICTALLBindingSource.Current :?> MDICTALL;
-            let url = System.String.Format(row.URL, HttpUtility.UrlEncode(this.wordTextBox.Text));
-            this.dictWebBrowser.Navigate(url);
-        );
+        this.searchButton.Click.AddHandler(new System.EventHandler(this.searchButton_Click));
         // 
         // wordTextBox
         // 
@@ -167,43 +93,111 @@ type MainForm() as this =
         this.dictWebBrowser.MinimumSize <- new System.Drawing.Size(30, 27);
         this.dictWebBrowser.Name <- "dictWebBrowser";
         this.dictWebBrowser.ScriptErrorsSuppressed <- true;
-        this.dictWebBrowser.Size <- new System.Drawing.Size(657, 333);
+        this.dictWebBrowser.Size <- new System.Drawing.Size(657, 364);
         this.dictWebBrowser.TabIndex <- 2;
+        // 
+        // panel2
+        // 
+        this.panel2.Controls.Add(this.langComboBox);
+        this.panel2.Controls.Add(this.dictComboBox);
+        this.panel2.Controls.Add(this.label2);
+        this.panel2.Controls.Add(this.label3);
+        this.panel2.Dock <- System.Windows.Forms.DockStyle.Bottom;
+        this.panel2.Location <- new System.Drawing.Point(0, 360);
+        this.panel2.Name <- "panel2";
+        this.panel2.Size <- new System.Drawing.Size(657, 37);
+        this.panel2.TabIndex <- 3;
+        // 
+        // langComboBox
+        // 
+        this.langComboBox.DataSource <- this.mLANGUAGEBindingSource;
+        this.langComboBox.DisplayMember <- "LANGNAME";
+        this.langComboBox.DropDownStyle <- System.Windows.Forms.ComboBoxStyle.DropDownList;
+        this.langComboBox.FormattingEnabled <- true;
+        this.langComboBox.Location <- new System.Drawing.Point(87, 5);
+        this.langComboBox.Margin <- new System.Windows.Forms.Padding(0, 4, 0, 4);
+        this.langComboBox.Name <- "langComboBox";
+        this.langComboBox.Size <- new System.Drawing.Size(180, 24);
+        this.langComboBox.TabIndex <- 6;
+        this.langComboBox.ValueMember <- "LANGID";
+        this.langComboBox.SelectionChangeCommitted.AddHandler(new System.EventHandler(this.langComboBox_SelectionChangeCommitted));
+        // 
+        // dictComboBox
+        // 
+        this.dictComboBox.DataSource <- this.mDICTALLBindingSource;
+        this.dictComboBox.DisplayMember <- "DICTNAME";
+        this.dictComboBox.DropDownStyle <- System.Windows.Forms.ComboBoxStyle.DropDownList;
+        this.dictComboBox.FormattingEnabled <- true;
+        this.dictComboBox.Location <- new System.Drawing.Point(364, 5);
+        this.dictComboBox.Margin <- new System.Windows.Forms.Padding(4);
+        this.dictComboBox.Name <- "dictComboBox";
+        this.dictComboBox.Size <- new System.Drawing.Size(180, 24);
+        this.dictComboBox.TabIndex <- 7;
+        this.dictComboBox.ValueMember <- "URL";
+        // 
+        // label2
+        // 
+        this.label2.AutoSize <- true;
+        this.label2.Location <- new System.Drawing.Point(5, 6);
+        this.label2.Margin <- new System.Windows.Forms.Padding(4, 0, 4, 0);
+        this.label2.Name <- "label2";
+        this.label2.Size <- new System.Drawing.Size(74, 16);
+        this.label2.TabIndex <- 4;
+        this.label2.Text <- "Language:";
+        // 
+        // label3
+        // 
+        this.label3.AutoSize <- true;
+        this.label3.Location <- new System.Drawing.Point(277, 6);
+        this.label3.Margin <- new System.Windows.Forms.Padding(4, 8, 4, 4);
+        this.label3.Name <- "label3";
+        this.label3.Size <- new System.Drawing.Size(79, 16);
+        this.label3.TabIndex <- 5;
+        this.label3.Text <- "Dictionary:";
         // 
         // MainForm
         // 
-        this.AutoScaleDimensions <- new System.Drawing.SizeF(9.0f, 16.0f);
+        this.AutoScaleDimensions <- new System.Drawing.SizeF(9.F, 16.F);
         this.AutoScaleMode <- System.Windows.Forms.AutoScaleMode.Font;
         this.ClientSize <- new System.Drawing.Size(657, 397);
+        this.Controls.Add(this.panel2);
         this.Controls.Add(this.dictWebBrowser);
         this.Controls.Add(this.panel1);
-        this.Controls.Add(this.tableLayoutPanel1);
-        this.Font <- new System.Drawing.Font("MS UI Gothic", 12.0f);
-        this.Margin <- new System.Windows.Forms.Padding(4)
+        this.Font <- new System.Drawing.Font("MS UI Gothic", 12.F);
+        this.Margin <- new System.Windows.Forms.Padding(4);
         this.Name <- "MainForm";
         this.StartPosition <- System.Windows.Forms.FormStartPosition.CenterScreen;
         this.Text <- "Form1";
-        this.Load.Add(fun _ ->
-            this.mLANGUAGEBindingSource.DataSource <- LollyDB.Languages_GetDataNonChinese();
-            langComboBox_SelectionChangeCommitted(null);
-        )
-        this.tableLayoutPanel1.ResumeLayout(false);
-        this.tableLayoutPanel1.PerformLayout();
+        this.Load.AddHandler(new System.EventHandler(this.MainForm_Load));
         ((this.mLANGUAGEBindingSource) :> (System.ComponentModel.ISupportInitialize)).EndInit();
         ((this.mDICTALLBindingSource) :> (System.ComponentModel.ISupportInitialize)).EndInit();
         this.panel1.ResumeLayout(false);
         this.panel1.PerformLayout();
+        this.panel2.ResumeLayout(false);
+        this.panel2.PerformLayout();
         this.ResumeLayout(false);
 
-    [<DefaultValue>]val mutable tableLayoutPanel1 : System.Windows.Forms.TableLayoutPanel
-    [<DefaultValue>]val mutable panel1 : System.Windows.Forms.Panel
-    [<DefaultValue>]val mutable wordTextBox : System.Windows.Forms.TextBox
-    [<DefaultValue>]val mutable label1 : System.Windows.Forms.Label
-    [<DefaultValue>]val mutable dictWebBrowser : System.Windows.Forms.WebBrowser
-    [<DefaultValue>]val mutable label2 : System.Windows.Forms.Label
-    [<DefaultValue>]val mutable label3 : System.Windows.Forms.Label
-    [<DefaultValue>]val mutable langComboBox : System.Windows.Forms.ComboBox
-    [<DefaultValue>]val mutable dictComboBox : System.Windows.Forms.ComboBox
-    [<DefaultValue>]val mutable mLANGUAGEBindingSource : System.Windows.Forms.BindingSource
-    [<DefaultValue>]val mutable mDICTALLBindingSource : System.Windows.Forms.BindingSource
-    [<DefaultValue>]val mutable searchButton : System.Windows.Forms.Button
+    [<DefaultValue>]val mutable private panel1 : System.Windows.Forms.Panel;
+    [<DefaultValue>]val mutable private wordTextBox : System.Windows.Forms.TextBox;
+    [<DefaultValue>]val mutable private label1 : System.Windows.Forms.Label;
+    [<DefaultValue>]val mutable private dictWebBrowser : System.Windows.Forms.WebBrowser;
+    [<DefaultValue>]val mutable private mLANGUAGEBindingSource : System.Windows.Forms.BindingSource;
+    [<DefaultValue>]val mutable private mDICTALLBindingSource : System.Windows.Forms.BindingSource;
+    [<DefaultValue>]val mutable private searchButton : System.Windows.Forms.Button;
+    [<DefaultValue>]val mutable private panel2 : System.Windows.Forms.Panel;
+    [<DefaultValue>]val mutable private langComboBox : System.Windows.Forms.ComboBox;
+    [<DefaultValue>]val mutable private dictComboBox : System.Windows.Forms.ComboBox;
+    [<DefaultValue>]val mutable private label2 : System.Windows.Forms.Label;
+    [<DefaultValue>]val mutable private label3 : System.Windows.Forms.Label;
+
+    member private this.MainForm_Load sender e =
+        this.mLANGUAGEBindingSource.DataSource <- LollyDB.Languages_GetDataNonChinese();
+        this.langComboBox_SelectionChangeCommitted(null, null) |> ignore;
+
+    member private this.langComboBox_SelectionChangeCommitted sender e =
+        this.mDICTALLBindingSource.DataSource <- LollyDB.DictAll_GetDataByLang(this.langComboBox.SelectedValue :?> int64);
+
+    member private this.searchButton_Click sender e =
+        let row = this.mDICTALLBindingSource.Current :?> MDICTALL;
+        let url = System.String.Format(row.URL, HttpUtility.UrlEncode(this.wordTextBox.Text));
+        this.dictWebBrowser.Navigate(url);
--- LollyWinForms/MainForm.Designer.cs
+++ LollyWinForms/MainForm.Designer.cs
@@ -29,98 +29,31 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.langComboBox = new System.Windows.Forms.ComboBox();
             this.mLANGUAGEBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.dictComboBox = new System.Windows.Forms.ComboBox();
             this.mDICTALLBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.searchButton = new System.Windows.Forms.Button();
             this.wordTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.dictWebBrowser = new System.Windows.Forms.WebBrowser();
-            this.tableLayoutPanel1.SuspendLayout();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.langComboBox = new System.Windows.Forms.ComboBox();
+            this.dictComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.mLANGUAGEBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.mDICTALLBindingSource)).BeginInit();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.ColumnCount = 4;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.langComboBox, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.dictComboBox, 3, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label3, 2, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 366);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 1;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(657, 31);
-            this.tableLayoutPanel1.TabIndex = 0;
-            // 
-            // langComboBox
-            // 
-            this.langComboBox.DataSource = this.mLANGUAGEBindingSource;
-            this.langComboBox.DisplayMember = "LANGNAME";
-            this.langComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.langComboBox.FormattingEnabled = true;
-            this.langComboBox.Location = new System.Drawing.Point(86, 4);
-            this.langComboBox.Margin = new System.Windows.Forms.Padding(4);
-            this.langComboBox.Name = "langComboBox";
-            this.langComboBox.Size = new System.Drawing.Size(180, 24);
-            this.langComboBox.TabIndex = 2;
-            this.langComboBox.ValueMember = "LANGID";
-            this.langComboBox.SelectionChangeCommitted += new System.EventHandler(this.langComboBox_SelectionChangeCommitted);
             // 
             // mLANGUAGEBindingSource
             // 
             this.mLANGUAGEBindingSource.DataSource = typeof(LollyShared.MLANGUAGE);
             // 
-            // dictComboBox
-            // 
-            this.dictComboBox.DataSource = this.mDICTALLBindingSource;
-            this.dictComboBox.DisplayMember = "DICTNAME";
-            this.dictComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.dictComboBox.FormattingEnabled = true;
-            this.dictComboBox.Location = new System.Drawing.Point(417, 4);
-            this.dictComboBox.Margin = new System.Windows.Forms.Padding(4);
-            this.dictComboBox.Name = "dictComboBox";
-            this.dictComboBox.Size = new System.Drawing.Size(180, 24);
-            this.dictComboBox.TabIndex = 3;
-            this.dictComboBox.ValueMember = "URL";
-            // 
             // mDICTALLBindingSource
             // 
             this.mDICTALLBindingSource.DataSource = typeof(LollyShared.MDICTALL);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(4, 8);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 8, 4, 4);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(74, 16);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Language:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(330, 8);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 8, 4, 4);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(79, 16);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "Dictionary:";
             // 
             // panel1
             // 
@@ -171,47 +104,105 @@
             this.dictWebBrowser.MinimumSize = new System.Drawing.Size(30, 27);
             this.dictWebBrowser.Name = "dictWebBrowser";
             this.dictWebBrowser.ScriptErrorsSuppressed = true;
-            this.dictWebBrowser.Size = new System.Drawing.Size(657, 333);
+            this.dictWebBrowser.Size = new System.Drawing.Size(657, 364);
             this.dictWebBrowser.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.langComboBox);
+            this.panel2.Controls.Add(this.dictComboBox);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 360);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(657, 37);
+            this.panel2.TabIndex = 3;
+            // 
+            // langComboBox
+            // 
+            this.langComboBox.DataSource = this.mLANGUAGEBindingSource;
+            this.langComboBox.DisplayMember = "LANGNAME";
+            this.langComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.langComboBox.FormattingEnabled = true;
+            this.langComboBox.Location = new System.Drawing.Point(87, 5);
+            this.langComboBox.Margin = new System.Windows.Forms.Padding(0, 4, 0, 4);
+            this.langComboBox.Name = "langComboBox";
+            this.langComboBox.Size = new System.Drawing.Size(180, 24);
+            this.langComboBox.TabIndex = 6;
+            this.langComboBox.ValueMember = "LANGID";
+            this.langComboBox.SelectionChangeCommitted += new System.EventHandler(this.langComboBox_SelectionChangeCommitted);
+            // 
+            // dictComboBox
+            // 
+            this.dictComboBox.DataSource = this.mDICTALLBindingSource;
+            this.dictComboBox.DisplayMember = "DICTNAME";
+            this.dictComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.dictComboBox.FormattingEnabled = true;
+            this.dictComboBox.Location = new System.Drawing.Point(364, 5);
+            this.dictComboBox.Margin = new System.Windows.Forms.Padding(4);
+            this.dictComboBox.Name = "dictComboBox";
+            this.dictComboBox.Size = new System.Drawing.Size(180, 24);
+            this.dictComboBox.TabIndex = 7;
+            this.dictComboBox.ValueMember = "URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 6);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Language:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(277, 6);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 8, 4, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Dictionary:";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(657, 397);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.dictWebBrowser);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.tableLayoutPanel1);
             this.Font = new System.Drawing.Font("MS UI Gothic", 12F);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.MainForm_Load);
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.mLANGUAGEBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.mDICTALLBindingSource)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.TextBox wordTextBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.WebBrowser dictWebBrowser;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ComboBox langComboBox;
-        private System.Windows.Forms.ComboBox dictComboBox;
         private System.Windows.Forms.BindingSource mLANGUAGEBindingSource;
         private System.Windows.Forms.BindingSource mDICTALLBindingSource;
         private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox langComboBox;
+        private System.Windows.Forms.ComboBox dictComboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
