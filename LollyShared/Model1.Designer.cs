ca45f175f9bd991b89de972428812ce130ec7a0f
zwvista <zwvista@msn.com>
2016-09-20 15:02:25 +0900

    db

--- LollyShared/Model1.Designer.cs
+++ LollyShared/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 コード生成がモデル 'C:\zw\programs\Lolly\LollyCS\LollyShared\Model1.edmx' に対して有効です。 
+﻿// T4 コード生成がモデル 'C:\zw\Programs\Lolly\LollyCS\LollyShared\Model1.edmx' に対して有効です。 
 // レガシ コード生成を有効にするには、'コード生成方法' デザイナー
 // プロパティの値を 'レガシ ObjectContext' に変更します。このプロパティは、デザイナーでモデルが開かれている場合に [プロパティ] ウィンドウ
 // で使用できます。
