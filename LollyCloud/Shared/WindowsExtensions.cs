494b8807b42ad9c31710d7fa0d5f9c8193a8838a
zwvista <zwvista@msn.com>
2019-07-11 17:46:17 +0900

    settings

--- LollyCloud/Shared/WindowsExtensions.cs
+++ LollyCloud/Shared/WindowsExtensions.cs
@@ -83,4 +83,29 @@ namespace LollyCloud
             throw new NotSupportedException();
         }
     }
+    // https://stackoverflow.com/questions/20707160/data-binding-int-property-to-enum-in-wpf
+    public class EnumConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+                              System.Globalization.CultureInfo culture)
+        {
+            int returnValue = 0;
+            if (parameter is Type)
+            {
+                returnValue = (int)Enum.Parse((Type)parameter, value.ToString());
+            }
+            return returnValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+                                  System.Globalization.CultureInfo culture)
+        {
+            Enum enumValue = default(Enum);
+            if (parameter is Type)
+            {
+                enumValue = (Enum)Enum.Parse((Type)parameter, value.ToString());
+            }
+            return enumValue;
+        }
+    }
 }
