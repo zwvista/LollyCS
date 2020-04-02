4d2dca4985e55505eaa1b2db9e385dc1dab8d5b5
zwvista <zwvista@msn.com>
2020-04-02 19:25:18 +0900

    Init

--- LollyCloud/UI/Words/WordsDictControl.xaml.cs
+++ LollyCloud/UI/Words/WordsDictControl.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using CefSharp;
 using LollyShared;
 using MSHTML;
+using System;
 using System.Diagnostics;
 using System.Linq;
 using System.Windows;
@@ -18,39 +19,50 @@ namespace LollyCloud
         public MDictReference Dict;
         public string Word = "";
         public SettingsViewModel vmSettings;
+        Action LoadAsync;
         public WordsDictControl()
         {
             InitializeComponent();
         }
-        public async void SearchWord(string word)
+        public void SearchWord(string word)
         {
-            if (string.IsNullOrEmpty(word)) return;
             Word = word;
             dictStatus = DictWebBrowserStatus.Ready;
             var url = Dict.UrlString(word, vmSettings.AutoCorrects.ToList());
-            if (Dict.DICTTYPENAME == "OFFLINE")
+            LoadAsync = async () =>
             {
-                wbDict.Load("about:blank");
-                var html = await vmSettings.client.GetStringAsync(url);
-                var str = Dict.HtmlString(html, word);
-                wbDict.LoadHtml(str);
-            }
-            else
-            {
-                wbDict.Load(url);
-                if (Dict.AUTOMATION != null)
-                    dictStatus = DictWebBrowserStatus.Automating;
-                else if (Dict.DICTTYPENAME == "OFFLINE-ONLINE")
-                    dictStatus = DictWebBrowserStatus.Navigating;
-            }
+                if (Dict.DICTTYPENAME == "OFFLINE")
+                {
+                    wbDict.Load("about:blank");
+                    var html = await vmSettings.client.GetStringAsync(url);
+                    var str = Dict.HtmlString(html, word);
+                    wbDict.LoadHtml(str);
+                }
+                else
+                {
+                    wbDict.Load(url);
+                    if (Dict.AUTOMATION != null)
+                        dictStatus = DictWebBrowserStatus.Automating;
+                    else if (Dict.DICTTYPENAME == "OFFLINE-ONLINE")
+                        dictStatus = DictWebBrowserStatus.Navigating;
+                }
+            };
+            DoLoadAsync();
         }
-
-        public async void wbDict_LoadingStateChanged(object sender, LoadingStateChangedEventArgs args)
+        void DoLoadAsync()
+        {
+            if (wbDict.IsBrowserInitialized)
+                LoadAsync();
+        }
+        void wbDict_IsBrowserInitializedChanged(object sender, DependencyPropertyChangedEventArgs e) => DoLoadAsync();
+        async void wbDict_LoadingStateChanged(object sender, LoadingStateChangedEventArgs args)
         {
             if (args.IsLoading) return;
             var frame = args.Browser.MainFrame;
             Dispatcher.Invoke(() => {
-                tbURL.Text = frame.Url;
+                var s = frame.Url;
+                if (!s.StartsWith("data"))
+                    tbURL.Text = s;
             });
             if (dictStatus == DictWebBrowserStatus.Ready) return;
             switch (dictStatus)
