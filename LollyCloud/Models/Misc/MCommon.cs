52d017873c7548ec67caa88cf865455e189f4f2d
zwvista <zwvista@msn.com>
2020-04-08 12:14:00 +0900

    Models

--- LollyCloud/Models/Misc/MCommon.cs
+++ LollyCloud/Models/Misc/MCommon.cs
@@ -15,22 +15,6 @@ namespace LollyCloud
             Label = l;
         }
     }
-    public interface MWordInterface
-    {
-        int LANGID { get; set; }
-        int WORDID { get; }
-        string WORD { get; set; }
-        string NOTE { get; set; }
-        int FAMIID { get; set; }
-        int LEVEL { get; set; }
-    }
-    public interface MPhraseInterface
-    {
-        int LANGID { get; set; }
-        int PHRASEID { get; }
-        string PHRASE { get; set; }
-        string TRANSLATION { get; set; }
-    }
     public class MCodes
     {
         public List<MCode> records { get; set; }
--- LollyCloud/Models/WPP/MWordPhrase.cs
+++ LollyCloud/Models/WPP/MWordPhrase.cs
@@ -5,6 +5,22 @@ using ReactiveUI.Fody.Helpers;
 
 namespace LollyCloud
 {
+    public interface MWordInterface
+    {
+        int LANGID { get; set; }
+        int WORDID { get; }
+        string WORD { get; set; }
+        string NOTE { get; set; }
+        int FAMIID { get; set; }
+        int LEVEL { get; set; }
+    }
+    public interface MPhraseInterface
+    {
+        int LANGID { get; set; }
+        int PHRASEID { get; }
+        string PHRASE { get; set; }
+        string TRANSLATION { get; set; }
+    }
     public class MWordsPhrases
     {
         public List<MWordPhrase> records { get; set; }
