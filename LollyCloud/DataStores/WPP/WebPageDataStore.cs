d485108f9c985e46947b397bd9831ffdb45e6373
zwvista <zwvista@msn.com>
2020-07-07 18:08:45 +0900

    WebPage

---
+++ LollyCloud/DataStores/WPP/WebPageDataStore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using Plugin.Connectivity;
+
+namespace LollyCloud
+{
+    public class WebPageDataStore : LollyDataStore<MWebPage>
+    {
+        public async Task<int> Create(MWebPage item) =>
+        await CreateByUrl($"WEBPAGES", item);
+        public async Task Update(MWebPage item) =>
+        Debug.WriteLine(await UpdateByUrl($"WEBPAGES/{item.ID}", JsonConvert.SerializeObject(item)));
+        public async Task Delete(int id) =>
+        Debug.WriteLine(await DeleteByUrl($"WEBPAGES/{id}"));
+    }
+}
---
+++ LollyCloud/Models/WPP/MWebPage.cs
@@ -0,0 +1,26 @@
+﻿using ReactiveUI;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using ReactiveUI.Fody.Helpers;
+
+namespace LollyCloud
+{
+    public class MWebPages
+    {
+        [JsonProperty("records")]
+        public List<MWebPage> Records { get; set; }
+    }
+    [JsonObject(MemberSerialization.OptIn)]
+    public class MWebPage : ReactiveObject
+    {
+        [JsonProperty]
+        [Reactive]
+        public int ID { get; set; }
+        [JsonProperty]
+        [Reactive]
+        public string TITLE { get; set; }
+        [JsonProperty]
+        [Reactive]
+        public string URL { get; set; }
+    }
+}
