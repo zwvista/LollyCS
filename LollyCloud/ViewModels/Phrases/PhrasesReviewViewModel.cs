5ab7ae7e689fc93d5308157bdceaa35ed3621ad6
zwvista <zwvista@msn.com>
2020-08-08 16:58:32 +0900

    review

--- LollyCloud/ViewModels/Phrases/PhrasesReviewViewModel.cs
+++ LollyCloud/ViewModels/Phrases/PhrasesReviewViewModel.cs
@@ -1,7 +1,11 @@
 ﻿using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace LollyCloud
 {
@@ -20,14 +24,38 @@ namespace LollyCloud
         public string CurrentPhrase => HasNext ? Items[Index].PHRASE : "";
         public bool IsTestMode => Options.Mode == ReviewMode.Test;
         public MReviewOptions Options { get; set; } = new MReviewOptions();
+        public IDisposable SubscriptionTimer;
+        public Action DoTestAction;
+        public bool IsSpeaking { get; set; }
+        [Reactive]
+        public string IndexString { get; set; }
+        [Reactive]
+        public Visibility IndexVisibility { get; set; }
+        [Reactive]
+        public Visibility CorrectVisibility { get; set; }
+        [Reactive]
+        public Visibility IncorrectVisibility { get; set; }
+        [Reactive]
+        public bool CheckEnabled { get; set; }
+        [Reactive]
+        public string PhraseTargetString { get; set; }
+        [Reactive]
+        public Visibility PhraseTargetVisibility { get; set; }
+        [Reactive]
+        public string TranslationString { get; set; }
+        [Reactive]
+        public string PhraseInputString { get; set; }
+        [Reactive]
+        public string CheckString { get; set; }
 
         // https://stackoverflow.com/questions/15907356/how-to-initialize-an-object-using-async-await-pattern
-        public PhrasesReviewViewModel(SettingsViewModel vmSettings, bool needCopy)
+        public PhrasesReviewViewModel(SettingsViewModel vmSettings, bool needCopy, Action doTestAction)
         {
             this.vmSettings = !needCopy ? vmSettings : vmSettings.ShallowCopy();
+            DoTestAction = doTestAction;
         }
 
-        public async void NewTest()
+        public async Task NewTest()
         {
             Items = await unitPhraseDS.GetDataByTextbookUnitPart(
                 vmSettings.SelectedTextbook, vmSettings.USUNITPARTFROM, vmSettings.USUNITPARTTO);
@@ -38,6 +66,12 @@ namespace LollyCloud
                 Items.Shuffle();
             CorrectIDs = new List<int>();
             Index = 0;
+            DoTest();
+            CheckString = IsTestMode ? "Check" : "Next";
+            if (Options.Mode == ReviewMode.ReviewAuto)
+                SubscriptionTimer = Observable.Interval(TimeSpan.FromSeconds(Options.Interval), RxApp.MainThreadScheduler).Subscribe(_ => Check());
+            else
+                SubscriptionTimer?.Dispose();
         }
         public void Next()
         {
@@ -48,12 +82,49 @@ namespace LollyCloud
                 Items = Items.Where(o => !CorrectIDs.Contains(o.ID)).ToList();
             }
         }
-        public void Check(string phraseInput)
+        public void Check()
         {
-            if (!HasNext) return;
-            var o = CurrentItem;
-            var isCorrect = o.PHRASE == phraseInput;
-            if (isCorrect) CorrectIDs.Add(o.ID);
+            if (!IsTestMode)
+            {
+                Next();
+                DoTest();
+            }
+            else if (CorrectVisibility != Visibility.Visible && IncorrectVisibility != Visibility.Visible)
+            {
+                PhraseInputString = vmSettings.AutoCorrectInput(PhraseInputString);
+                PhraseTargetVisibility = Visibility.Hidden;
+                if (PhraseInputString == CurrentPhrase)
+                    CorrectVisibility = Visibility.Visible;
+                else
+                    IncorrectVisibility = Visibility.Visible;
+                CheckString = "Next";
+                if (!HasNext) return;
+                var o = CurrentItem;
+                var isCorrect = o.PHRASE == PhraseInputString;
+                if (isCorrect) CorrectIDs.Add(o.ID);
+            }
+            else
+            {
+                Next();
+                DoTest();
+                CheckString = "Check";
+            }
+        }
+        public void DoTest()
+        {
+            IndexVisibility = HasNext ? Visibility.Visible : Visibility.Hidden;
+            CorrectVisibility = Visibility.Hidden;
+            IncorrectVisibility = Visibility.Hidden;
+            CheckEnabled = HasNext;
+            PhraseTargetString = CurrentPhrase;
+            TranslationString = CurrentItem?.TRANSLATION ?? "";
+            PhraseTargetVisibility = !IsTestMode ? Visibility.Visible : Visibility.Hidden;
+            PhraseInputString = "";
+            DoTestAction?.Invoke();
+            if (HasNext)
+                IndexString = $"{Index + 1}/{Count}";
+            else if (Options.Mode == ReviewMode.ReviewAuto)
+                SubscriptionTimer?.Dispose();
         }
     }
 }
--- LollyCloud/Views/Phrases/PhrasesReviewControl.xaml.cs
+++ LollyCloud/Views/Phrases/PhrasesReviewControl.xaml.cs
@@ -15,7 +15,6 @@ namespace LollyCloud
     {
         public PhrasesReviewViewModel vm { get; set; }
         public SettingsViewModel vmSettings => vm.vmSettings;
-        DispatcherTimer _timer;
 
         public PhrasesReviewControl()
         {
@@ -28,88 +27,28 @@ namespace LollyCloud
 
         public async Task OnSettingsChanged()
         {
-            _timer?.Stop();
-            vm = new PhrasesReviewViewModel(MainWindow.vmSettings, needCopy: true);
+            DataContext = vm = new PhrasesReviewViewModel(MainWindow.vmSettings, needCopy: true, () =>
+            {
+                tbPhraseInput.Focus();
+                if (vm.HasNext && vm.IsSpeaking)
+                    App.Speak(vm.vmSettings, vm.CurrentPhrase);
+            });
             btnNewTest_Click(null, null);
         }
-        void DoTest()
-        {
-            var b = vm.HasNext;
-            lblIndex.Visibility = b ? Visibility.Visible : Visibility.Hidden;
-            lblCorrect.Visibility = Visibility.Hidden;
-            lblIncorrect.Visibility = Visibility.Hidden;
-            btnCheck.IsEnabled = b;
-            lblPhraseTarget.Content = vm.CurrentPhrase;
-            lblTranslation.Content = vm.CurrentItem?.TRANSLATION ?? "";
-            lblPhraseTarget.Visibility = !vm.IsTestMode ? Visibility.Visible : Visibility.Hidden;
-            tbPhraseInput.Text = "";
-            tbPhraseInput.Focus();
-            if (b)
-            {
-                lblIndex.Content = $"{vm.Index + 1}/{vm.Count}";
-                //if isspeaking {
-                //    synth.startspeaking(vm.currentword);
-                //}
-            }
-            else if (vm.Options.Mode == ReviewMode.ReviewAuto)
-                _timer.Stop();
-        }
-
-        void btnNewTest_Click(object sender, RoutedEventArgs e)
+        async void btnNewTest_Click(object sender, RoutedEventArgs e)
         {
             var dlg = new ReviewOptionsDlg(Window.GetWindow(this), vm.Options);
             if (dlg.ShowDialog() == true)
-            {
-                vm.NewTest();
-                DoTest();
-                btnCheck.Content = vm.IsTestMode ? "Check" : "Next";
-                if (vm.Options.Mode == ReviewMode.ReviewAuto)
-                {
-                    _timer = new DispatcherTimer();
-                    _timer.Interval = new TimeSpan(0, 0, 3);
-                    _timer.Tick += (s, e2) => btnCheck_Click(sender, e);
-                    _timer.Start();
-                }
-                else
-                    _timer?.Stop();
-            }
+                await vm.NewTest();
         }
 
-        void btnCheck_Click(object sender, RoutedEventArgs e)
-        {
-            if (!vm.IsTestMode)
-            {
-                vm.Next();
-                DoTest();
-            }
-            else if (!lblCorrect.IsVisible && lblIncorrect.IsVisible)
-            {
-                tbPhraseInput.Text = vmSettings.AutoCorrectInput(tbPhraseInput.Text);
-                lblPhraseTarget.Visibility = Visibility.Hidden;
-                if (tbPhraseInput.Text == vm.CurrentPhrase)
-                    lblCorrect.Visibility = Visibility.Visible;
-                else
-                    lblIncorrect.Visibility = Visibility.Visible;
-                btnCheck.Content = "Next";
-                vm.Check(tbPhraseInput.Text);
-            }
-            else
-            {
-                vm.Next();
-                DoTest();
-                btnCheck.Content = "Check";
-            }
-        }
+        void btnCheck_Click(object sender, RoutedEventArgs e) =>
+            vm.Check();
 
         void tbPhraseInput_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key != Key.Return) return;
-            btnCheck_Click(sender, null);
-        }
-
-        void chkSpeak_Click(object sender, RoutedEventArgs e)
-        {
-
+            vm.Check();
         }
     }
 }
