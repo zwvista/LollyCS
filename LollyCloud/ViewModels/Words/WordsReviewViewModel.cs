255b9ce9eb90a5942744e878ef47c77f327011b7
zwvista <zwvista@msn.com>
2020-08-08 14:50:54 +0900

    review

--- LollyCloud/ViewModels/Words/WordsReviewViewModel.cs
+++ LollyCloud/ViewModels/Words/WordsReviewViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace LollyCloud
 {
@@ -18,20 +21,52 @@ namespace LollyCloud
         public List<int> CorrectIDs { get; set; }
         [Reactive]
         public int Index { get; set; }
-        public string IndexCount => $"{Index}/{Count}";
         public bool HasNext => Index < Count;
         public MUnitWord CurrentItem => HasNext ? Items[Index] : null;
         public string CurrentWord => HasNext ? Items[Index].WORD : "";
         public bool IsTestMode => Options.Mode == ReviewMode.Test;
         public MReviewOptions Options { get; set; } = new MReviewOptions();
+        public IDisposable SubscriptionTimer;
+        public Action DoTestAction;
+        public bool IsSpeaking { get; set; }
+        [Reactive]
+        public string IndexString { get; set; }
+        [Reactive]
+        public Visibility IndexVisibility { get; set; }
+        [Reactive]
+        public Visibility CorrectVisibility { get; set; }
+        [Reactive]
+        public Visibility IncorrectVisibility { get; set; }
+        [Reactive]
+        public string AccuracyString { get; set; }
+        [Reactive]
+        public Visibility AccuracyVisibility { get; set; }
+        [Reactive]
+        public bool CheckEnabled { get; set; }
+        [Reactive]
+        public string WordTargetString { get; set; }
+        [Reactive]
+        public string NoteTargetString { get; set; }
+        [Reactive]
+        public Visibility WordTargetVisibility { get; set; }
+        [Reactive]
+        public Visibility NoteTargetVisibility { get; set; }
+        [Reactive]
+        public string TranslationString { get; set; }
+        [Reactive]
+        public string WordInputString { get; set; }
+        [Reactive]
+        public string CheckString { get; set; }
 
-        public WordsReviewViewModel(SettingsViewModel vmSettings, bool needCopy)
+        public WordsReviewViewModel(SettingsViewModel vmSettings, bool needCopy, Action doTestAction)
         {
             this.vmSettings = !needCopy ? vmSettings : vmSettings.ShallowCopy();
+            DoTestAction = doTestAction;
         }
 
         public async Task NewTest()
         {
+            SubscriptionTimer?.Dispose();
             Items = await unitWordDS.GetDataByTextbookUnitPart(
                 vmSettings.SelectedTextbook, vmSettings.USUNITPARTFROM, vmSettings.USUNITPARTTO);
             int nFrom = Count * (Options.GroupSelected - 1) / Options.GroupCount;
@@ -43,6 +78,12 @@ namespace LollyCloud
                 Items.Shuffle();
             CorrectIDs = new List<int>();
             Index = 0;
+            await DoTest();
+            CheckString = IsTestMode ? "Check" : "Next";
+            if (Options.Mode == ReviewMode.ReviewAuto)
+                SubscriptionTimer = Observable.Interval(TimeSpan.FromSeconds(Options.Interval), RxApp.MainThreadScheduler).Subscribe(async _ => await Check());
+            else
+                SubscriptionTimer?.Dispose();
         }
         public void Next()
         {
@@ -60,15 +101,60 @@ namespace LollyCloud
             var html = await vmSettings.client.GetStringAsync(url);
             return HtmlTransformService.ExtractTextFromHtml(html, DictTranslation.TRANSFORM, "", (text, _) => text);
         }
-        public async Task Check(string wordInput)
+        public async Task Check()
         {
-            if (!HasNext) return;
-            var o = CurrentItem;
-            var isCorrect = o.WORD == wordInput;
-            if (isCorrect) CorrectIDs.Add(o.ID);
-            var o2 = await wordFamiDS.Update(o.WORDID, isCorrect);
-            o.CORRECT = o2.CORRECT;
-            o.TOTAL = o2.TOTAL;
+            if (!IsTestMode)
+            {
+                Next();
+                await DoTest();
+            }
+            else if (CorrectVisibility != Visibility.Visible && IncorrectVisibility != Visibility.Visible)
+            {
+                WordInputString = vmSettings.AutoCorrectInput(WordInputString);
+                WordTargetVisibility = Visibility.Visible;
+                NoteTargetVisibility = Visibility.Visible;
+                if (WordInputString == CurrentWord)
+                    CorrectVisibility = Visibility.Visible;
+                else
+                    IncorrectVisibility = Visibility.Visible;
+                CheckString = "Next";
+                if (!HasNext) return;
+                var o = CurrentItem;
+                var isCorrect = o.WORD == WordInputString;
+                if (isCorrect) CorrectIDs.Add(o.ID);
+                var o2 = await wordFamiDS.Update(o.WORDID, isCorrect);
+                o.CORRECT = o2.CORRECT;
+                o.TOTAL = o2.TOTAL;
+            }
+            else
+            {
+                Next();
+                await DoTest();
+                CheckString = "Check";
+            }
+        }
+        public async Task DoTest()
+        {
+            IndexVisibility = HasNext ? Visibility.Visible : Visibility.Hidden;
+            CorrectVisibility = Visibility.Hidden;
+            IncorrectVisibility = Visibility.Hidden;
+            AccuracyVisibility = IsTestMode && HasNext ? Visibility.Visible : Visibility.Hidden;
+            CheckEnabled = HasNext;
+            WordTargetString = CurrentWord;
+            NoteTargetString = CurrentItem?.NOTE ?? "";
+            WordTargetVisibility = !IsTestMode ? Visibility.Visible : Visibility.Hidden;
+            NoteTargetVisibility = !IsTestMode ? Visibility.Visible : Visibility.Hidden;
+            TranslationString = "";
+            WordInputString = "";
+            DoTestAction?.Invoke();
+            if (HasNext)
+            {
+                IndexString = $"{Index + 1}/{Count}";
+                AccuracyString = CurrentItem.ACCURACY;
+                TranslationString = await GetTranslation();
+            }
+            else if (Options.Mode == ReviewMode.ReviewAuto)
+                SubscriptionTimer?.Dispose();
         }
     }
 }
--- LollyCloud/Views/Words/WordsReviewControl.xaml.cs
+++ LollyCloud/Views/Words/WordsReviewControl.xaml.cs
@@ -15,7 +15,6 @@ namespace LollyCloud
     {
         public WordsReviewViewModel vm { get; set; }
         public SettingsViewModel vmSettings => vm.vmSettings;
-        DispatcherTimer _timer;
 
         public WordsReviewControl()
         {
@@ -28,37 +27,17 @@ namespace LollyCloud
 
         public async Task OnSettingsChanged()
         {
-            _timer?.Stop();
-            vm = new WordsReviewViewModel(MainWindow.vmSettings, needCopy: true);
-            btnNewTest_Click(null, null);
-        }
-
-        async Task DoTest()
-        {
-            var b = vm.HasNext;
-            lblIndex.Visibility = b ? Visibility.Visible : Visibility.Hidden;
-            lblCorrect.Visibility = Visibility.Hidden;
-            lblIncorrect.Visibility = Visibility.Hidden;
-            lblAccuracy.Visibility = vm.IsTestMode && b ? Visibility.Visible : Visibility.Hidden;
-            btnCheck.IsEnabled = b;
-            lblWordTarget.Content = vm.CurrentWord;
-            lblNoteTarget.Content = vm.CurrentItem?.NOTE ?? "";
-            lblWordTarget.Visibility = !vm.IsTestMode ? Visibility.Visible : Visibility.Hidden;
-            lblNoteTarget.Visibility = !vm.IsTestMode ? Visibility.Visible : Visibility.Hidden;
-            tbTranslation.Text = "";
-            tbWordInput.Text = "";
-            tbWordInput.Focus();
-            if (b)
+            vm = new WordsReviewViewModel(MainWindow.vmSettings, needCopy: true, () =>
             {
-                lblIndex.Content = $"{vm.Index + 1}/{vm.Count}";
-                lblAccuracy.Content = vm.CurrentItem.ACCURACY;
-                //if isspeaking {
-                //    synth.startspeaking(vm.currentword);
-                //}
-                tbTranslation.Text = await vm.GetTranslation();
-            }
-            else if (vm.Options.Mode == ReviewMode.ReviewAuto)
-                _timer.Stop();
+                tbWordInput.Focus();
+                if (vm.HasNext)
+                {
+                    if (vm.IsSpeaking) {
+                        App.Speak(vm.vmSettings, vm.CurrentWord);
+                    }
+                }
+            });
+            await vm.NewTest();
         }
 
         async void btnNewTest_Click(object sender, RoutedEventArgs e)
@@ -67,51 +46,16 @@ namespace LollyCloud
             if (dlg.ShowDialog() == true)
             {
                 await vm.NewTest();
-                await DoTest();
-                btnCheck.Content = vm.IsTestMode ? "Check" : "Next";
-                if (vm.Options.Mode == ReviewMode.ReviewAuto)
-                {
-                    _timer = new DispatcherTimer();
-                    _timer.Interval = new TimeSpan(0, 0, 3);
-                    _timer.Tick += (s, e2) => btnCheck_Click(sender, e);
-                    _timer.Start();
-                }
-                else
-                    _timer?.Stop();
             }
         }
 
-        async void btnCheck_Click(object sender, RoutedEventArgs e)
-        {
-            if (!vm.IsTestMode)
-            {
-                vm.Next();
-                await DoTest();
-            }
-            else if (!lblCorrect.IsVisible && !lblIncorrect.IsVisible)
-            {
-                tbWordInput.Text = vmSettings.AutoCorrectInput(tbWordInput.Text);
-                lblWordTarget.Visibility = Visibility.Visible;
-                lblNoteTarget.Visibility = Visibility.Visible;
-                if (tbWordInput.Text == vm.CurrentWord)
-                    lblCorrect.Visibility = Visibility.Visible;
-                else
-                    lblIncorrect.Visibility = Visibility.Visible;
-                btnCheck.Content = "Next";
-                await vm.Check(tbWordInput.Text);
-            }
-            else
-            {
-                vm.Next();
-                await DoTest();
-                btnCheck.Content = "Check";
-            }
-        }
+        async void btnCheck_Click(object sender, RoutedEventArgs e) =>
+            await vm.Check();
 
-        void tbWordInput_KeyDown(object sender, KeyEventArgs e)
+        async void tbWordInput_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.Return && !(vm.IsTestMode && string.IsNullOrEmpty(tbWordInput.Text)))
-                btnCheck_Click(sender, null);
+            if (e.Key == Key.Return && !(vm.IsTestMode && string.IsNullOrEmpty(vm.WordInputString)))
+                await vm.Check();
         }
 
         void chkSpeak_Click(object sender, RoutedEventArgs e)
