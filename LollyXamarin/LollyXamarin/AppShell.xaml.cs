e24046da1c9bb5c3eff819445aa8f12197771d84
zwvista <zwvista@msn.com>
2020-08-18 18:35:38 +0900

    Xamarin

---
+++ LollyXamarin/LollyXamarin/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using LollyXamarin.ViewModels;
+using LollyXamarin.Views;
+using Xamarin.Forms;
+
+namespace LollyXamarin
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
