43cb50bb8df77b13629dd8c2f10ad6bff626e2a1
zwvista <zwvista@msn.com>
2020-08-28 18:35:29 +0900

    WordsUnitPage

--- LollyXamarin/LollyXamarin/AppShell.xaml.cs
+++ LollyXamarin/LollyXamarin/AppShell.xaml.cs
@@ -1,25 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using LollyCloud;
 using LollyXamarin.ViewModels;
 using LollyXamarin.Views;
 using Xamarin.Forms;
 
 namespace LollyXamarin
 {
-    public partial class AppShell : Xamarin.Forms.Shell
+    public partial class AppShell : Shell
     {
+        public static SettingsViewModel vmSettings = new SettingsViewModel();
         public AppShell()
         {
             InitializeComponent();
             Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
             Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
             // https://stackoverflow.com/questions/56748966/hide-tabbar-in-xamarin-forms-shell
-            Shell.SetTabBarIsVisible(this, false);
+            SetTabBarIsVisible(this, false);
+            Task.Run(() => vmSettings.GetData()).Wait();
         }
 
         private async void OnMenuItemClicked(object sender, EventArgs e)
         {
-            await Shell.Current.GoToAsync("//LoginPage");
+            await Current.GoToAsync("//LoginPage");
         }
     }
 }
---
+++ LollyXamarin/LollyXamarin/Views/Words/WordsUnitPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using LollyCloud;
+
+namespace LollyXamarin.Views
+{
+    public partial class WordsUnitPage : ContentPage
+    {
+        WordsUnitViewModel vm;
+
+        public WordsUnitPage()
+        {
+            InitializeComponent();
+            BindingContext = vm = new WordsUnitViewModel(AppShell.vmSettings, true, false);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+    }
+}
