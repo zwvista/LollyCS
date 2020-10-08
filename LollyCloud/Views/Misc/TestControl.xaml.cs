bcebdce60a0741fa296393228d46ef24070671db
zwvista <zwvista@msn.com>
2020-10-08 17:54:37 +0900

    zwvista

--- LollyCloud/Views/Misc/TestControl.xaml.cs
+++ LollyCloud/Views/Misc/TestControl.xaml.cs
@@ -211,5 +211,65 @@ namespace LollyCloud
             }
         }
 
+        async void TestForZwvistaWordPress1()
+        {
+            var reg1 = new Regex(@"<h2 class=""post-title"">\n\s+<a href=""(https://zwvista.wordpress.com/.+?)"" rel=""bookmark"">【日语句型】(.+?)</a>\n\s+</h2>");
+            var client = new HttpClient();
+            var lines2 = new List<string>();
+            for (int i = 0; i < 100; i++)
+            {
+                string html;
+                try
+                {
+                    html = await client.GetStringAsync($"https://zwvista.wordpress.com/category/%E6%97%A5%E8%AF%AD%E5%8F%A5%E5%9E%8B/page/{i}/");
+                }
+                catch (Exception ex)
+                {
+                    break;
+                }
+                var ms = reg1.Matches(html).Cast<Match>().ToList();
+                foreach (var m in ms)
+                {
+                    var url = m.Groups[1].Value;
+                    var title = m.Groups[2].Value;
+                    var s = url + delim + title;
+                    lines2.Add(s);
+                }
+            }
+            File.WriteAllLines("b.txt", lines2);
+        }
+        async void TestForZwvistaWordPress2()
+        {
+            var lines = File.ReadAllLines("b.txt");
+            var storewp = new WebPageDataStore();
+            var storept = new PatternDataStore();
+            var storeptwp = new PatternWebPageDataStore();
+            foreach (var s in lines)
+            {
+                var a = s.Split(new[] { delim }, StringSplitOptions.RemoveEmptyEntries);
+                string url = a[0], title = a[1];
+                var pt = new MPattern
+                {
+                    LANGID = 2,
+                    PATTERN = title,
+                    TAGS = "zwvista",
+                };
+                var wp = new MWebPage
+                {
+                    TITLE = "【日语句型】" + title,
+                    URL = url,
+                };
+                var ptid = await storept.Create(pt);
+                var wpid = await storewp.Create(wp);
+                var ptwp = new MPatternWebPage
+                {
+                    PATTERNID = ptid,
+                    WEBPAGEID = wpid,
+                    SEQNUM = 1,
+                };
+                await storeptwp.Create(ptwp);
+            }
+        }
+
     }
 }
