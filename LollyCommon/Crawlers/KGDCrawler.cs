49e850a870ba1b51e57b883ad2a989dfc4e11f78
zwvista <zwvista@msn.com>
2020-11-02 17:45:43 +0900

    KGDCrawler

--- LollyCommon/Crawlers/KGDCrawler.cs
+++ LollyCommon/Crawlers/KGDCrawler.cs
@@ -15,18 +15,24 @@ namespace LollyCommon.Crawlers
     {
         public override async Task Step1()
         {
+            var home = "http://koreangrammaticalforms.com/";
             var client = new HttpClient();
-            var reg1 = new Regex(@"<span id=""ENTRYNAME"">(.+?)</span> ");
+            var html = await client.GetStringAsync($"{home}index.php");
+            var reg1 = new Regex(@"<a href=""javascript: GetPage\('(.+?)'\);"">.+?</a>");
+            var reg2 = new Regex(@"<li><a href=""(.+?)"" target=""_blank"">(.+?)</a></li>");
+            var ms = reg1.Matches(html);
             var lines2 = new List<string>();
-            for (int i = 1; i <= 10000; i++)
+            foreach (Match m in ms)
             {
-                var url = $"http://koreangrammaticalforms.com/entry.php?eid={i:D10}";
-                var html = await client.GetStringAsync(url);
-                var m = reg1.Match(html);
-                if (!m.Success) break;
-                var title = m.Groups[1].Value.Trim();
-                var s = url + delim + title;
-                lines2.Add(s);
+                var html2 = await client.GetStringAsync($"{home}{m.Groups[1].Value}");
+                var ms2 = reg2.Matches(html2);
+                foreach (Match m2 in ms2)
+                {
+                    var url = home + m2.Groups[1].Value;
+                    var title = m2.Groups[2].Value.Trim();
+                    var s = url + delim + title;
+                    lines2.Add(s);
+                }
             }
             File.WriteAllLines("b.txt", lines2);
         }
@@ -45,7 +51,7 @@ namespace LollyCommon.Crawlers
                 {
                     LANGID = 7,
                     PATTERN = title,
-                    TAGS = "MaiBlog",
+                    TAGS = "KGD",
                 };
                 var wp = new MWebPage
                 {
