62f479291b92f7d94bc17e4a4cf5c3d43be72f47
趙偉 <zwvista@msn.com>
2021-04-22 06:56:25 +0900

    review 1

---
+++ LollyXamarin/LollyXamarin/Views/Phrases/PhrasesReviewPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using LollyCommon;
+using Plugin.Clipboard;
+using Xamarin.Essentials;
+
+namespace LollyXamarin
+{
+    public partial class PhrasesReviewPage : ContentPage
+    {
+
+        public PhrasesReviewPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+        void ToolbarItemNewTest_Clicked(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ LollyXamarin/LollyXamarin/Views/Words/WordsReviewPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using LollyCommon;
+using Plugin.Clipboard;
+using Xamarin.Essentials;
+
+namespace LollyXamarin
+{
+    public partial class WordsReviewPage : ContentPage
+    {
+
+        public WordsReviewPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+        void ToolbarItemNewTest_Clicked(object sender, EventArgs e)
+        {
+        }
+    }
+}
