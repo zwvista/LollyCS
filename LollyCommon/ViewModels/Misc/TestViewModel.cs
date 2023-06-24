5bea36d2cfffeb65b9d5c2b992358f879dfaa128
cc-y-cho <y.cho@capsulecorp.co.jp>
2023-06-25 05:10:05 +0900

    crawlers 2

--- LollyCommon/ViewModels/Misc/TestViewModel.cs
+++ LollyCommon/ViewModels/Misc/TestViewModel.cs
@@ -1,13 +1,17 @@
 ﻿using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
+using ReactiveUI.Validation.Extensions;
+using ReactiveUI.Validation.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive;
 using System.Reactive.Linq;
+using System.Reflection;
 
 namespace LollyCommon
 {
-    public class TestViewModel : ReactiveObject
+    public class TestViewModel : ReactiveValidationObject
     {
         [Reactive]
         public string SelectedLang { get; set; }
@@ -17,16 +21,27 @@ namespace LollyCommon
         public List<string> Crawlers { get; set; }
         [Reactive]
         public string SelectedStep { get; set; }
+        public ReactiveCommand<Unit, Unit> ExecuteCommand { get; }
 
         public TestViewModel()
         {
             this.WhenAnyValue(x => x.SelectedLang).Subscribe(v =>
             {
-                Crawlers = string.IsNullOrEmpty(v) ? new List<string>() :
+                Crawlers = v == null ? new List<string>() :
                 GetType().Assembly.GetTypes()
                 .Where(t => t.Namespace == "LollyCommon.Crawlers.Patterns." + v && t.IsPublic)
                 .Select(t => t.Name).ToList();
             });
+            this.ValidationRule(x => x.SelectedLang, v => !string.IsNullOrWhiteSpace(v), "SelectedLang must not be empty");
+            this.ValidationRule(x => x.SelectedCrawler, v => !string.IsNullOrWhiteSpace(v), "SelectedCrawler must not be empty");
+            this.ValidationRule(x => x.SelectedStep, v => !string.IsNullOrWhiteSpace(v), "SelectedStep must not be empty");
+            ExecuteCommand = ReactiveCommand.Create(() =>
+            {
+                var t = GetType().Assembly.GetType($"LollyCommon.Crawlers.Patterns.{SelectedLang}.{SelectedCrawler}");
+                var i = Activator.CreateInstance(t);
+                var m = t.GetMethod(SelectedStep);
+                m.Invoke(i, new object[] {});
+            }, this.IsValid());
         }
     }
 }
