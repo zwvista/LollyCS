3850e6bbcd9afdc4cf165e75326d998a29a29222
趙偉 <zwvista@msn.com>
2024-05-04 11:58:01 +0900

    iOS simulator crash

--- LollyMaui/Platforms/iOS/AppDelegate.cs
+++ LollyMaui/Platforms/iOS/AppDelegate.cs
@@ -6,5 +6,6 @@ namespace LollyMaui
     public class AppDelegate : MauiUIApplicationDelegate
     {
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+        // https://stackoverflow.com/questions/78218327/got-a-sigabrt-while-executing-native-code-when-i-tap-in-an-entry
     }
 }
